1dc7cfb34399445d51d06c483a6974c2255fb655
Froschi1860 <fabianf96@googlemail.com>
2023-10-04 23:02:50 +0200

    implemented function to query for all cases of one user

--- RiskAssessmentProtoype/app/src/main/java/com/example/riskassessmentprotoype/database/queryModels.kt
+++ RiskAssessmentProtoype/app/src/main/java/com/example/riskassessmentprotoype/database/queryModels.kt
@@ -1,6 +1,9 @@
 package com.example.riskassessmentprotoype.database
 
 import android.database.sqlite.SQLiteDatabase
+import androidx.core.database.getFloatOrNull
+import androidx.core.database.getIntOrNull
+import androidx.core.database.getShortOrNull
 import java.util.Date
 import java.util.LinkedList
 
@@ -33,8 +36,7 @@ data class Case(
     val gender: String,
     val givenNames: String,
     val lastName: String,
-    val parents: List<InsertParent>,
-    val userId: Long,
+    val parents: LinkedList<Parent>,
     val neglectRisk: Boolean? = null,
     val neglectScore: Float? = null,
     val neglectEstimation: Float? = null,
@@ -60,8 +62,61 @@ data class Answer(
     val questionId: Int
 )
 
+fun getCasesByUser(db: SQLiteDatabase, userId: Long): List<Case> {
+    val caseList = LinkedList<Case>()
+    // get case data
+    val cursorCase = db.query("Cases", null, "user_id LIKE ?", arrayOf(userId.toString()), null, null, null)
+    with (cursorCase) {
+        while (moveToNext()) {
+            var neglectRisk: Boolean? = null
+            val neglectRiskInt = getIntOrNull(getColumnIndexOrThrow("neglect_risk"))
+            if (neglectRiskInt != null) neglectRisk = neglectRiskInt == 1
+            var pcaRisk: Boolean? = null
+            val pcaRiskInt = getIntOrNull(getColumnIndexOrThrow("pca_risk"))
+            if (pcaRiskInt != null) pcaRisk = pcaRiskInt == 1
+            caseList.add(Case(
+                id = getLong(getColumnIndexOrThrow("case_id")),
+                personnr = getString(getColumnIndexOrThrow("personnr")),
+                email = getString(getColumnIndexOrThrow("email")),
+                gender = getString(getColumnIndexOrThrow("gender")),
+                givenNames = getString(getColumnIndexOrThrow("given_names")),
+                lastName = getString(getColumnIndexOrThrow("last_name")),
+                neglectRisk = neglectRisk,
+                neglectScore = getFloatOrNull(getColumnIndex("neglect_score")),
+                neglectEstimation = getFloatOrNull(getColumnIndex("neglect_estimation")),
+                pcaRisk = pcaRisk,
+                pcaScore = getFloatOrNull(getColumnIndex("pca_score")),
+                pcaEstimation = getFloatOrNull(getColumnIndex("pca_estimation")),
+                parents = LinkedList<Parent>()
+            ))
+        }
+    }
+    // get Parents
+    for (case in caseList) {
+        val cursorParents = db.rawQuery(
+            "SELECT Parents.parent_id, Parents.given_names, Parents.last_name, Parents.gender " +
+                    "FROM Parent_to_Case " +
+                    "INNER JOIN Parents ON Parent_to_Case.parent_id=Parents.parent_id " +
+                    "WHERE Parent_to_Case.case_id LIKE ?",
+            arrayOf(case.id.toString())
+        )
+        with (cursorParents) {
+            while (moveToNext()) {
+                case.parents.add(Parent(
+                    getLong(getColumnIndexOrThrow("parent_id")),
+                    getString(getColumnIndexOrThrow("given_names")),
+                    getString(getColumnIndexOrThrow("last_name")),
+                    getString(getColumnIndexOrThrow("gender")),
+                ))
+            }
+        }
+    }
+    // add list of parents
+    return caseList
+}
+
 fun getAllUsernames(db: SQLiteDatabase): List<String> {
-    var usernameList = LinkedList<String>()
+    val usernameList = LinkedList<String>()
     val cursor = db.query("Users", arrayOf("username"), null, null, null, null, "username ASC")
     with (cursor) {
         while (cursor.moveToNext()) {
@@ -81,6 +136,7 @@ fun getPwByUsername(db: SQLiteDatabase, username: String): String? {
     }
     return pw
 }
+
 fun getUserByUsername(db: SQLiteDatabase, username: String): User? {
     var user: User? = null
     val cursor = db.query("Users", null, "username LIKE ?", arrayOf(username), null, null, null)
