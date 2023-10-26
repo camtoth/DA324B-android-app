3ffe0d9dd35d8c1470cfb822adc542ec444d32d4
smallpotato <smallpotato6035@gmail.com>
2023-10-26 21:05:02 +0200

    read questions from db

--- RiskAssesmentApp/app/src/main/java/com/example/riskassesmentapp/MainActivity.kt
+++ RiskAssesmentApp/app/src/main/java/com/example/riskassesmentapp/MainActivity.kt
@@ -178,7 +178,7 @@ fun MyApp() {
                         }
                     }
                     if (isLoggedIn) {
-                        AssessmentScreen(navController).Content()
+                        AssessmentScreen(navController, dbConnection).Content()
                     }
                 }
                 composable("settings") {
--- RiskAssesmentApp/app/src/main/java/com/example/riskassesmentapp/screens/AssesmentScreen.kt
+++ RiskAssesmentApp/app/src/main/java/com/example/riskassesmentapp/screens/AssesmentScreen.kt
@@ -10,7 +10,12 @@ import com.example.riskassesmentapp.db.QuestionWithAnswer
 import com.example.riskassesmentapp.db.getAllQuestions
 import com.example.riskassesmentapp.ui.composables.Assessment
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import java.net.IDN
 import java.util.LinkedList
 
@@ -18,17 +23,12 @@ class AssessmentScreen(private val navController: NavController, private val dbC
     private var questionsList = LinkedList<Question>()
     @Composable
     fun Content() {
-        var scope = rememberCoroutineScope()
-        questionsList.add(Question(id = 0, titleEn = "Question title", textEn = "To what extent do you see the child as a problem?", titleSe = "no hablo espaNol", textSe = "Swedish lul"))
-        questionsList.add(Question(id = 1, titleEn = "Question title", textEn = "Was the pregnancy unplanned?", titleSe = "no hablo espaNol", textSe = "Swedish lul", rNeglect = 0.2f,  rPca = 0.7f))
-        questionsList.add(Question(id = 2, titleEn = "Question title", textEn = "Does the cooperation between you and the child work well?", titleSe = "no hablo espaNol", textSe = "Swedish lul", rNeglect = 0.2f,  rPca = 0.7f))
-        questionsList.add(Question(id = 3, titleEn = "Question title", textEn = "Do you use physical punishment in parenting?", titleSe = "no hablo espaNol", textSe = "Swedish lul", rNeglect = 0.2f,  rPca = 0.7f))
-        //FetchQuestionsFromDB(scope)
-        Assessment(questionsList, 0, dbConnection) //TODO: get questions from DB and parentId from previous screen
+        FetchQuestionsFromDB()
+        Assessment(questionsList, 0, dbConnection) //TODO: get parentId from previous screen
     }
 
-    private fun FetchQuestionsFromDB (scope: CoroutineScope){
-        scope.launch {
+    private fun FetchQuestionsFromDB (){
+        runBlocking {
             questionsList = getAllQuestions(dbConnection)
         }
     }
